122651c1bd32f28843d0523560d9589ebb2f61d8
Cole Moore <Cartisim@icloud.com>
2022-04-05 18:34:42 +0800

    importing SwiftCrypto

--- Sources/NeedleTailKit/IRCKit/Extensions/Data+Extension.swift
+++ Sources/NeedleTailKit/IRCKit/Extensions/Data+Extension.swift
@@ -1,37 +1,36 @@
 import Foundation
-#if canImport(CommonCrypto)
-import CommonCrypto
-#endif
+import Crypto
 
-extension Data{
-    
-    /// Creates sha256 string from Data
-    /// - Returns: SHA256 Hash
-    internal func sha256() -> String {
-        return hexStringFromData(input: digest(input: self as NSData))
-    }
-    
-    /// Digest Data as an input on the `Data` Type
-    /// - Parameter input: `NSData`
-    /// - Returns: return the digested data as `NSData`
-    private func digest(input : NSData) -> NSData {
-        let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
-        var hash = [UInt8](repeating: 0, count: digestLength)
-        CC_SHA256(input.bytes, UInt32(input.length), &hash)
-        return NSData(bytes: hash, length: digestLength)
-    }
-    
-    /// Creates a Hex String from our Digested `NSData`
-    /// - Parameter input: `NSData`
-    /// - Returns: A hexString from the `Digest` of `NSData`
-    private func hexStringFromData(input: NSData) -> String {
-        var bytes = [UInt8](repeating: 0, count: input.length)
-        input.getBytes(&bytes, length: input.length)
-        
-        var hexString = ""
-        for byte in bytes {
-            hexString += String(format:"%02x", UInt8(byte))
-        }
-        return hexString
-    }
-}
+
+//extension Data{
+//    
+//    /// Creates sha256 string from Data
+//    /// - Returns: SHA256 Hash
+//    internal func sha256() -> String {
+//        return hexStringFromData(input: digest(input: self as NSData))
+//    }
+//    
+//    /// Digest Data as an input on the `Data` Type
+//    /// - Parameter input: `NSData`
+//    /// - Returns: return the digested data as `NSData`
+//    private func digest(input : NSData) -> NSData {
+//        let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
+//        var hash = [UInt8](repeating: 0, count: digestLength)
+//        CC_SHA256(input.bytes, UInt32(input.length), &hash)
+//        return NSData(bytes: hash, length: digestLength)
+//    }
+//    
+//    /// Creates a Hex String from our Digested `NSData`
+//    /// - Parameter input: `NSData`
+//    /// - Returns: A hexString from the `Digest` of `NSData`
+//    private func hexStringFromData(input: NSData) -> String {
+//        var bytes = [UInt8](repeating: 0, count: input.length)
+//        input.getBytes(&bytes, length: input.length)
+//        
+//        var hexString = ""
+//        for byte in bytes {
+//            hexString += String(format:"%02x", UInt8(byte))
+//        }
+//        return hexString
+//    }
+//}
