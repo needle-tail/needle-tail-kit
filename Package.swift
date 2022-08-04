88bcdf576671e523889806d7412f210caf23429f
Cole Moore <Cartisim@icloud.com>
2022-08-04 23:19:31 +0300

    using forked packages

--- Package.swift
+++ Package.swift
@@ -21,13 +21,13 @@ let package = Package(
             targets: ["NeedleTailHelpers"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.13.0"),
-        .package(url: "https://github.com/apple/swift-nio.git", from: "2.27.0"),
-        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.8.0"),
-        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.7.1"),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.0.5")),
-        .package(url: "https://github.com/orlandos-nl/CypherTextKit.git", branch: "feature/async-await"),
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.13.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.41.1"),
+        .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.12.1"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.21.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.1.3")),
+        .package(url: "https://github.com/needle-tail/CypherTextKit.git", branch: "feature/async-await"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.1.0")
     ],
     targets: [
         .target(
