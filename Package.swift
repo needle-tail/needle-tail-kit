9de41a8149df06900c1fc283a534d5a3bc2f1c11
Cole Moore <Cartisim@icloud.com>
2022-08-12 17:11:15 +0300

    updated package.swift for 5.6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,8 +6,8 @@ import PackageDescription
 let package = Package(
     name: "needle-tail-kit",
     platforms: [
-        .macOS(.v13),
-        .iOS(.v16),
+        .macOS(.v12),
+        .iOS(.v15),
     ],
     products: [
         .library(
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.12.1"),
         .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.21.0"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.1.3")),
-        .package(url: "https://github.com/needle-tail/CypherTextKit.git", branch: "feature/async-await"),
+        .package(url: "https://github.com/needle-tail/CypherTextKit.git", revision: "2b0569683624b69865779f7f5fe70a2fcf3bbaf8"),
         .package(url: "https://github.com/apple/swift-crypto.git", from: "2.1.0")
     ],
     targets: [
