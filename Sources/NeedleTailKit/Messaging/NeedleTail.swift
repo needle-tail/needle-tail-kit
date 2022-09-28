7cffcba03781cda8a1a8e50283aa73d61a2bba96
Cole Moore <Cartisim@icloud.com>
2022-09-28 20:23:02 +0300

    removed unused extension on View

--- Sources/NeedleTailKit/Messaging/NeedleTail.swift
+++ Sources/NeedleTailKit/Messaging/NeedleTail.swift
@@ -690,34 +690,3 @@ public func makeP2PFactories() -> [P2PTransportClientFactory] {
 }
 
 #endif
-extension View {
-    #if os(iOS)
-    func onBackground(_ f: @escaping () -> Void) -> some View {
-        self.onReceive(
-            NotificationCenter.default.publisher(for: UIApplication.willResignActiveNotification),
-            perform: { _ in f() }
-        )
-    }
-    
-    func onForeground(_ f: @escaping () -> Void) -> some View {
-        self.onReceive(
-            NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification),
-            perform: { _ in f() }
-        )
-    }
-    #else
-    func onBackground(_ f: @Sendable @escaping () async-> Void) -> some View {
-        self.onReceive(
-            NotificationCenter.default.publisher(for: NSApplication.willResignActiveNotification),
-            perform: { _ in Task { await f() } }
-        )
-    }
-    
-    func onForeground(_ f: @Sendable @escaping () async-> Void) -> some View {
-        self.onReceive(
-            NotificationCenter.default.publisher(for: NSApplication.didBecomeActiveNotification),
-            perform: { _ in Task { await f() } }
-        )
-    }
-    #endif
-}
